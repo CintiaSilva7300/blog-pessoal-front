67843bcc7bf0125fd17b880c5f5b51da16ad7204
CintiaSilva7300 <88053580+CintiaSilva7300@users.noreply.github.com>
2024-02-27 22:19:42 -0300

    remove import que n estava sendo usado

--- src/componentes/temas/listaTemas/ListaTemas.tsx
+++ src/componentes/temas/listaTemas/ListaTemas.tsx
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { Dna } from 'react-loader-spinner';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
-import Tema from '../../../models/Tema';
-import { buscar } from '../../../services/Service';
-import CardTemas from '../cardTemas/CardTemas';
-import { toastAlerta } from '../../../util/toastAlerta';
+import { useContext, useEffect, useState } from "react";
+import { Dna } from "react-loader-spinner";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthContext";
+import Tema from "../../../models/Tema";
+import { buscar } from "../../../services/Service";
+import CardTemas from "../cardTemas/CardTemas";
+import { toastAlerta } from "../../../util/toastAlerta";
 
 function ListaTemas() {
   const [temas, setTemas] = useState<Tema[]>([]);
@@ -17,21 +17,21 @@ function ListaTemas() {
 
   async function buscarTemas() {
     try {
-      await buscar('/temas', setTemas, {
+      await buscar("/temas", setTemas, {
         headers: { Authorization: token },
       });
     } catch (error: any) {
-      if (error.toString().includes('403')) {
-        toastAlerta('O token expirou, favor logar novamente', 'info')
-        handleLogout()
+      if (error.toString().includes("403")) {
+        toastAlerta("O token expirou, favor logar novamente", "info");
+        handleLogout();
       }
     }
   }
 
   useEffect(() => {
-    if (token === '') {
-      toastAlerta('Você precisa estar logado', 'info');
-      navigate('/login');
+    if (token === "") {
+      toastAlerta("Você precisa estar logado", "info");
+      navigate("/login");
     }
   }, [token]);
 
